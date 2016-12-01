148cdf6f5b735a35a08a1a65f7f45ae2fe96a8ac
masterUNG <phrombutr@gmail.com>
2016-12-01 14:41:55 +0700

    Update 1dec

--- app/src/main/java/appewtc/masterung/testnotification/MainActivity.java
+++ app/src/main/java/appewtc/masterung/testnotification/MainActivity.java
@@ -5,8 +5,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
 import android.view.View;
 
